b52f3f83b65ed02b60baaccaf3fa30dbb3b57905
zzqq2199 <zhouquanjs@qq.com>
2020-06-07 20:03:39 +0800

    add wrapper

--- output/naive_random.hpp
+++ output/naive_random.hpp
@@ -5,6 +5,14 @@
 #include <iostream>
 using namespace std;
 
+
+#ifdef __CUDACC__
+#define CUDA_HOSTDEV __host__ __device__
+#else
+#define CUDA_HOSTDEV
+#endif
+
+
 template<typename T>
 class Random{
 private:
@@ -12,16 +20,19 @@ private:
     uint32_t _c;
     uint32_t _x;
 public:
+    CUDA_HOSTDEV
     Random(const uint32_t seed){
         _a=1103515245;
         _c=12345;
         _x = seed;
     }
+    CUDA_HOSTDEV
     uint32_t rand(){
         _x = _a*_x + _c;
         return _x;
     }
 
+    CUDA_HOSTDEV
     T operator()(T lower_bound, T upper_bound){
         return static_cast<T>(rand()%(upper_bound-lower_bound+1))+lower_bound;
     }
@@ -36,6 +47,7 @@ private:
     uint32_t _max;
     double _max_double;
 public:
+    CUDA_HOSTDEV
     Random(const uint32_t seed){
         _a=1103515245;
         _c=12345;
@@ -43,11 +55,13 @@ public:
         _max = ~0;
         _max_double = static_cast<double>(_max);
     }
+    CUDA_HOSTDEV
     uint32_t rand(){
         _x = _a*_x + _c;
         return _x;
     }
 public:
+    CUDA_HOSTDEV
     float operator()(float lower_bound, float upper_bound){
         return static_cast<float>(rand()/(_max_double/(upper_bound-lower_bound)))+lower_bound;
     }
--- unit.py
+++ unit.py
@@ -13,6 +13,11 @@ bitwidth_of_typename = {
     'void': 0,
     'lambda_func': 0
 }
+type_alias = f"""
+#define uint8 uint8_t
+#define uint32 uint32_t
+#define int32 int32_t
+"""
 header = f"""
 #include <chrono>
 #include <stdint.h>
@@ -28,9 +33,7 @@ header = f"""
 #include "get_policy_general.h"
 using namespace zq_cpp_lib::operate_memory;
 
-#define uint8 uint8_t
-#define uint32 uint32_t
-#define int32 int32_t
+{type_alias}
 """
 
 
@@ -57,10 +60,16 @@ class Utype(): # difer from keyword type
                 self.bitwidth = sympy.Symbol(bitwidth)
         elif ret == 'param_type':
             self.bitwidth = 0
-    def generate_call(self):
+    def elem_type_str(self):
+        '''
+        @return: type of element(no *)
+        '''
         name = self.name
         if name == 'uint':
             name = 'uint8'
+        return name
+    def generate_call(self):
+        name = self.elem_type_str()
         if self.is_vector:
             name+='*'
         return name
@@ -151,7 +160,8 @@ class Func():
         for v in self.vars:
             if v not in self.params:
                 v = self.vars[v]
-                vars_expr += f"\t{v}\n"
+                if type(v) != Lambda_func:
+                    vars_expr += f"\t{v}\n"
         translated_expr = self.translated()
         ans = f"""Func[name:{self.name}
 Params:
@@ -164,10 +174,179 @@ Params:
     def aggregate_rate(self):
         return 1
 
-    def dump_to_file(self):
+    def dump_body(self):
         with open(f"output/{self.name}_body.h",'w') as f:
             f.write(header)
             f.write(self.translated())
+
+    def dump_wrapper(self):
+        with open(f"output/{self.name}_wrapper.h", 'w') as f:
+            f.write(self.generate_wrapper_mxnet())
+    def dump_register(self):
+        with open(f"output/{self.name}.cc", 'w') as f:
+            f.write(self.generate_register_mxnet_cpu())
+        with open(f"output/{self.name}.cu", 'w') as f:
+            f.write(self.generate_register_mxnet_gpu())
+
+    def generate_register_mxnet_gpu(self):
+        header = f'''#include "{self.name}_wrapper.h"
+#include <thrust/execution_policy.h>
+'''
+        register_op = f'''
+NNVM_REGISTER_OP(_contrib_{self.name})
+.set_attr<FCompute>("FCompute<gpu>", {self.name}<gpu, thrust::cuda_cub::par_t::stream_attachment_type>)
+;
+'''
+        ans = f'''
+{header}
+namespace mxnet{{
+namespace op{{
+{register_op}
+}}
+}}
+'''
+        return ans
+
+    def generate_register_mxnet_cpu(self):
+        header = f'''#include "{self.name}_wrapper.h"
+#include <thrust/execution_policy.h>
+#include <thrust/system/omp/execution_policy.h>
+'''
+        vectors = []
+        for v in self.params:
+            v:Variable = self.params[v]
+            if v.utype.is_vector:
+                vectors.append(v.name)
+        register_parameter = f"DMLC_REGISTER_PARAMETER({self.name}_param);\n"
+        register_op = f"NNVM_REGISTER_OP(_contrib_{self.name})\n"
+        register_op += f".set_attr_parser(ParamParser<{self.name}_param>)\n"
+        register_op += f".set_num_inputs({len(vectors)})\n"
+        register_op += f".set_num_outputs(0)\n"
+        register_op += f'''.set_attr<nnvm::FListInputNames>("FListInputNames",
+    [](const NodeAttrs& attrs){{
+        return std::vector<std::string>{{ {','.join([f'"{v}"' for v in vectors])} }};
+}}
+)
+'''
+        register_op += f'.set_attr<mxnet::FInferShape>("FInferShape", {self.name}_shape)\n'
+        register_op += f'.set_attr<nnvm::FInferType>("FInferType", {self.name}_type)\n'
+        for v in vectors:
+            register_op += f'.add_argument("{v}", "NDArray", "array")\n'
+        register_op += f'.add_arguments({self.name}_param::__FIELDS__())\n'
+        register_op += f';\n\n'
+
+        register_op += f'NNVM_REGISTER_OP(_contrib_{self.name})\n'
+        register_op += f'.set_attr<FCompute>("FCompute<cpu>", {self.name}<cpu, thrust::detail::host_t>)\n'
+        register_op += f";\n\n"
+        
+        ans = f'''
+{header}
+namespace mxnet{{
+namespace op{{
+{register_parameter}
+{register_op}
+}}
+}}
+'''
+        return ans
+
+    def generate_wrapper_mxnet(self):
+        header = f"""#include <mxnet/operator_util.h>
+#include <vector>
+#include "../mshadow_op.h"
+#include "../mxnet_op.h"
+#include "../operator_common.h"
+#include "../elemwise_op_common.h"
+#include "../tensor/init_op.h"
+
+#include "{self.name}_body.h"
+#include "get_stream_mxnet.h"
+
+{type_alias}
+"""
+        type_to_id = {
+            'float':0,
+            'uint8':3
+        }
+        param_define = ""
+        dmlc_declare = ""
+        vector_data = ""
+        type_infer = ""
+        vector_id = 0
+        for v in self.params:
+            v:Variable = self.params[v]
+            if v.utype.is_vector == False:
+                param_define += f"\t{v.utype.generate_call()} {v.name};\n"
+                dmlc_declare += f'\tDMLC_DECLARE_FIELD({v.name}).describe("Describe something for this parameter");\n'
+            else:
+                ele_type = v.utype.elem_type_str()
+                type_id = type_to_id[ele_type]
+                type_infer += f'\tCHECK_EQ(in_attrs->at({vector_id}), {type_id}) << "type of {v.name} should be {ele_type}";\n'
+                vector_data += f"\tconst TBlob &{v.name}_data = inputs[{vector_id}];\n"
+                vector_data += f"\t{v.utype.generate_call()} {v.name} = reinterpret_cast<{v.utype.generate_call()}>({v.name}_data.dptr<{ele_type}>());\n"
+                vector_data += f"\tuint32 _{v.name}_size = {v.name}_data.Size();\n"
+                vector_id += 1
+        dmlc_declare = f'''DMLC_DECLARE_PARAMETER({self.name}_param){{
+{dmlc_declare}
+}};
+'''
+        param_define = f'''struct {self.name}_param : public dmlc::Parameter<{self.name}_param> {{
+{param_define}
+{dmlc_declare}
+}};
+'''
+        shape_infer = f'''inline bool {self.name}_shape(const nnvm::NodeAttrs& attrs, mxnet::ShapeVector* in_attrs, mxnet::ShapeVector* out_attrs){{
+    return true;
+}};
+'''
+        type_infer = f'''inline bool {self.name}_type(const nnvm::NodeAttrs& attrs, std::vector<int>* in_attrs, std::vector<int>* out_attrs){{
+{type_infer}
+    return true;
+}};
+'''
+        body_call = ""
+        for v in self.params:
+            v:Variable = self.params[v]
+            if v.utype.is_vector:
+                body_call += f"\t\t{v.name},\n"
+                body_call += f"\t\t_{v.name}_size,\n"
+            else:
+                body_call += f"\t\tparam.{v.name},\n"
+        body_call += f"\t\tpolicy,\n"
+        body_call += f"\t\tstream\n"
+        body_call = f'''{self.name}_body<policy_t>(
+{body_call}
+    );
+'''
+
+        wrapper_func = f'''template <typename xpu, typename policy_t>
+void {self.name}(const nnvm::NodeAttrs& attrs,
+                    const OpContext& ctx,
+                    const std::vector<TBlob>& inputs,
+                    const std::vector<OpReqType>& req,
+                    const std::vector<TBlob>& outputs){{
+    using namespace mxnet_op;
+    const {self.name}_param& param = nnvm::get<{self.name}_param>(attrs.parsed);
+    void* stream = get_stream<policy_t>::get(ctx);
+    policy_t policy = get_policy<policy_t>::get(stream);
+    {vector_data}
+    {body_call}
+}};
+'''
+        ans = f"""
+{header}
+namespace mxnet{{
+namespace op{{
+{param_define}
+{shape_infer}
+{type_infer}
+{wrapper_func}
+}}
+}}
+"""
+        return ans
+        
+
 
     def translated(self):
         ans = ""
